ae42992dea3516b1a0424e58504f9433686e4075
Sravana Bommisetty <sravana.bommisetty@outlook.com>
2020-09-29 05:28:10 +0530

    Add project files.

---
+++ addition1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, result;
+            Console.WriteLine("Enter a value");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b value");
+            b = Convert.ToInt32(Console.ReadLine());
+            result = a + b;
+            Console.WriteLine("The sum is={0}", result);
+            Console.ReadKey();
+        }
+    }
+}
